574e0b9f912a8fa6f945a215bd676bc1b1cfbc35
rdorier <rdorier39@gmail.com>
2026-05-07 12:24:37 +0200

    little code improvement

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,57 @@
+use anyhow::{Context, Result, anyhow};
+use image::{ImageReader, RgbaImage};
+use ndarray::{Array2, array};
 use std::env;
-use image::{RgbaImage, ImageReader};
-use anyhow::{anyhow, Context, Result};
-use ndarray::{array, Array2};
 
 mod image_transforms;
 pub use crate::image_transforms::*;
 
-
 #[derive(PartialEq)]
 enum CLICommandValidity {
     ValidCommand,
-    InvalidCommand
+    InvalidCommand,
 }
 
-
-fn sobel_filter(img : RgbaImage ) -> RgbaImage  {
+fn sobel_filter(img: RgbaImage) -> RgbaImage {
     // Detect edges using Sobel algorithm.
 
     // sobel kernel to use to find intensity peaks in image : the intensity peaks represent edges
     // this kernel highlight vertical edges
-    let sobel_x_matrix = array![
-        [-1., 0., 1.],
-        [-2., 0., 2.],
-        [-1., 0., 1.],
-    ];
+    let sobel_x_matrix = array![[-1., 0., 1.], [-2., 0., 2.], [-1., 0., 1.],];
     // this kernel highlight horizontal edges
-    let sobel_y_matrix = array![
-        [-1., -2., -1.],
-        [ 0.,  0.,  0.],
-        [ 1.,  2.,  1.],
-    ];
-    
+    let sobel_y_matrix = array![[-1., -2., -1.], [0., 0., 0.], [1., 2., 1.],];
+
     // convert image as intensity array (grayscale value)
     let img_as_gray_array = rgba_to_gray_array_conversion(&img);
     let (height, width) = img_as_gray_array.dim();
 
     let mut edges_array = Array2::<f32>::zeros((height, width));
-    
+
     // convolve intensity array with kernel
-    for i in 1..((height - 1) as i32){
+    for i in 1..((height - 1) as i32) {
         for j in 1..((width - 1) as i32) {
             let mut sumx = 0.0;
             let mut sumy = 0.0;
 
             for p in -1i32..=1 {
                 for q in -1i32..=1 {
-                    let y : usize = (i + p).try_into().unwrap();
-                    let x : usize = (j + q).try_into().unwrap();
-                    
-                    let kernel_x : usize = (p + 1).try_into().unwrap();
-                    let kernel_y : usize = (q + 1).try_into().unwrap();
+                    let y: usize = (i + p).try_into().unwrap();
+                    let x: usize = (j + q).try_into().unwrap();
 
-                    sumx = sumx + (img_as_gray_array[[y,x]] * sobel_x_matrix[[kernel_x, kernel_y]]);
-                    sumy = sumy + (img_as_gray_array[[y,x]] * sobel_y_matrix[[kernel_x, kernel_y]]);
+                    let kernel_x: usize = (p + 1).try_into().unwrap();
+                    let kernel_y: usize = (q + 1).try_into().unwrap();
+
+                    sumx += img_as_gray_array[[y, x]] * sobel_x_matrix[[kernel_x, kernel_y]];
+                    sumy += (img_as_gray_array[[y, x]] * sobel_y_matrix[[kernel_x, kernel_y]]);
                 }
             }
             edges_array[[i as usize, j as usize]] = ((sumx * sumx) + (sumy * sumy)).sqrt();
         }
     }
-    
+
     // convert array into a grayscale image
-    let edges_image = gray_array_to_rgba_conversion(&edges_array);
-
-    edges_image
+    gray_array_to_rgba_conversion(&edges_array)
 }
-
 
 fn main() -> Result<()> {
     // check if CLI command enter by user is valid
@@ -75,7 +62,9 @@ fn main() -> Result<()> {
 
     // check parameters number
     if args.len() < 4 {
-        return Err(anyhow!("Not enough arguments given to the CLI. You must at least specify an input image, an output location and an operation to perform."));
+        return Err(anyhow!(
+            "Not enough arguments given to the CLI. You must at least specify an input image, an output location and an operation to perform."
+        ));
     }
 
     // get path of image to treat
@@ -91,7 +80,6 @@ fn main() -> Result<()> {
         .with_guessed_format()
         .context("Could not guess image format")?;
 
-
     // decode input image and convert it to RGBA
     let img = reader.decode().context("Failed to decode image")?;
     let mut img = img.to_rgba8();
@@ -104,7 +92,7 @@ fn main() -> Result<()> {
         _ => {
             println!("Unknown requested operation : {operation}");
             command_validity = CLICommandValidity::InvalidCommand
-        },
+        }
     }
 
     if command_validity == CLICommandValidity::ValidCommand {
@@ -112,8 +100,7 @@ fn main() -> Result<()> {
         img.save(output_file)
             .with_context(|| format!("Failed to save output file '{}'", output_file))?;
         println!("Resulting image saved at location : {output_file}");
-    }
-    else {
+    } else {
         // exit if invalid operation requested
         println!("Invalid operation requested ! Nothing to do here !")
     }
