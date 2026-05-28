fed28c90d31383a2cdd91795d97bf82f90506c33
rdorier <rdorier39@gmail.com>
2026-05-28 15:58:22 +0200

    improve gaussian blur performance by using separability property of the gaussian function

--- src/filters/src/gaussian_blur.rs
+++ src/filters/src/gaussian_blur.rs
@@ -3,8 +3,10 @@ use image::{Pixel, Rgba, RgbaImage};
 use std::f64::consts::PI;
 
 /// Filter to blur an image using the Gaussian function
+/// Use separability property of the gaussian function to apply a 1-dimensional kernel on both dimensions :
+/// G(x,y) = G(x) . G(y)
 pub struct GaussianBlur {
-    kernel: Vec<Vec<f64>>,
+    kernel: Vec<f64>,
     kernel_size: usize,
 }
 
@@ -26,15 +28,14 @@ impl GaussianBlur {
             kernel_size += 1
         };
 
-        // row major matrix representing gaussian kernel to apply to pixels : first index represents the row, second one the columns
-        let mut kernel = vec![vec![0.0; kernel_size]; kernel_size];
+        // one-dimensional kernel that will be applied both on row and on column of the image to blur
+        let mut kernel = vec![0.0; kernel_size];
 
-        for y in 0..kernel_size {
-            for x in 0..kernel_size {
-                // 2D Gaussian function: G(x, y) = (1 / (2 * pi * sigma^2)) * exp(-(x^2 + y^2) / (2 * sigma^2))
-                kernel[y][x] = (1.0 / (2.0 * PI * sigma * sigma))
-                    * (-((x * x + y * y) as f64) / (2.0 * sigma * sigma)).exp();
-            }
+        for x in 0..kernel_size {
+            // 2D Gaussian function: G(x, y) = (1 / (2 * pi * sigma^2)) * exp(-(x^2 + y^2) / (2 * sigma^2))
+            // is transformed in a 1-dimensional function
+            kernel[x] = (1.0 / (2.0 * PI * sigma * sigma))
+                * (-((x * x) as f64) / (2.0 * sigma * sigma)).exp();
         }
 
         Ok(Self {
@@ -46,16 +47,59 @@ impl GaussianBlur {
 
 impl Filtering for GaussianBlur {
     fn filter(&self, input_img: RgbaImage) -> RgbaImage {
-        // TODO : use separability property of the gaussian function to apply two 1-dimensional (one on rows, the other one on columns)
-
         let (width, height) = input_img.dimensions();
 
+        let half_kernel_size = self.kernel_size / 2; // use to center kernel on pixel
+
+        let mut temp_image = RgbaImage::new(width, height);
         let mut blurred_image = RgbaImage::new(width, height);
 
-        for y in 1..height {
-            for x in 1..width {
-                let half_kernel_size = self.kernel_size / 2; // use to center kernel on pixel
+        // TODO : mutualize code to avoid similar doouble loop ? For example,s omething like :
+        // let mut blurred_image = apply_1d_kernel(input_img, x)
+        // blurred_image = apply_1d_kernel(blurred_image, y)
 
+        // apply kernel horizontaly
+        for y in 0..height {
+            for x in 0..width {
+                let mut blurred_pixel: Rgba<u8> = Rgba([0; 4]);
+
+                for channel_index in 0..4 {
+                    let mut gaussian_sum: f64 = 0.0;
+                    let mut accumulated_weight = 0.0;
+
+                    for kx in 0..self.kernel_size {
+                        // calculate coordinates of current neighbour pixel (shifted by hald kernel size to center align Kernel with current pixel calculated (x,y))
+                        let neighbour_x = (x as i128) + (kx as i128) - (half_kernel_size as i128);
+
+                        if neighbour_x >= 0 && neighbour_x < width as i128 {
+                            let weight = self.kernel[kx];
+
+                            gaussian_sum += (input_img
+                                .get_pixel_checked(neighbour_x as u32, y as u32)
+                                .unwrap()
+                                .channels()[channel_index]
+                                as f64)
+                                * weight;
+                            accumulated_weight += weight;
+                        }
+                    }
+
+                    // normalize weight to avoid having darker or brighter image
+                    let normalized_gaussian_value = if accumulated_weight > 0.0 {
+                        gaussian_sum / accumulated_weight
+                    } else {
+                        0.0
+                    };
+                    blurred_pixel[channel_index] = normalized_gaussian_value as u8;
+                }
+
+                temp_image[(x, y)] = blurred_pixel;
+            }
+        }
+
+        // apply kernel vertically
+        for y in 0..height {
+            for x in 0..width {
                 let mut blurred_pixel: Rgba<u8> = Rgba([0; 4]);
 
                 for channel_index in 0..4 {
@@ -63,28 +107,19 @@ impl Filtering for GaussianBlur {
                     let mut accumulated_weight = 0.0;
 
                     for ky in 0..self.kernel_size {
-                        for kx in 0..self.kernel_size {
-                            // calculate coordinates of current neighbour pixel (shifted by hald kernel size to center align Kernel with current pixel calculated (x,y))
-                            let neighbour_x =
-                                (x as i128) + (kx as i128) - (half_kernel_size as i128);
-                            let neighbour_y =
-                                (y as i128) + (ky as i128) - (half_kernel_size as i128);
+                        // calculate coordinates of current neighbour pixel (shifted by hald kernel size to center align Kernel with current pixel calculated (x,y))
+                        let neighbour_y = (y as i128) + (ky as i128) - (half_kernel_size as i128);
 
-                            if neighbour_x >= 0
-                                && neighbour_x < width as i128
-                                && neighbour_y >= 0
-                                && neighbour_y < height as i128
-                            {
-                                let weight = self.kernel[ky][kx];
+                        if neighbour_y >= 0 && neighbour_y < height as i128 {
+                            let weight = self.kernel[ky];
 
-                                gaussian_sum += (input_img
-                                    .get_pixel_checked(neighbour_x as u32, neighbour_y as u32)
-                                    .unwrap()
-                                    .channels()[channel_index]
-                                    as f64)
-                                    * weight;
-                                accumulated_weight += weight;
-                            }
+                            gaussian_sum += (temp_image
+                                .get_pixel_checked(x as u32, neighbour_y as u32)
+                                .unwrap()
+                                .channels()[channel_index]
+                                as f64)
+                                * weight;
+                            accumulated_weight += weight;
                         }
                     }
 
